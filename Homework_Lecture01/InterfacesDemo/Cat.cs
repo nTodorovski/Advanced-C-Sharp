72ad3e5349cccc987b32ef2e2290e3514f52c3f1
Nikola Todorovski <nTodorovski@users.noreply.github.com>
2019-03-29 14:57:27 +0100

    Homework 1

---
+++ Homework_Lecture01/InterfacesDemo/Cat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InterfacesDemo
+{
+    public class Cat : IPredator
+    {
+        public int AttackSpeed { get; set; }
+        public string Name { get; set; }
+
+        public void Attack(IPray pray)
+        {
+            if (this.AttackSpeed > pray.FleeSpeed)
+            {
+                Console.WriteLine(this.Name + " catches " + pray.Name);
+            }
+            else
+            {
+                Console.WriteLine(pray.Name + " escapes from " + this.Name);
+            }
+        }
+    }
+}
---
+++ Homework_Lecture01/InterfacesDemo/Gazella.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InterfacesDemo
+{
+    public class Gazella : IPray
+    {
+        public int FleeSpeed { get; set; }
+        public string Name { get; set; }
+
+        public void Flee()
+        {
+            Console.WriteLine(this.Name + " flees away...");
+        }
+    }
+}
