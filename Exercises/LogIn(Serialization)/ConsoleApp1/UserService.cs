ebee07b85425a1176346691c06919ef5a1a5dfbb
Nikola Todorovski <nTodorovski@users.noreply.github.com>
2019-05-07 21:19:01 +0200

    Password change missing

--- Exercises/LogIn(Serialization)/ConsoleApp1/UserService.cs
+++ Exercises/LogIn(Serialization)/ConsoleApp1/UserService.cs
@@ -87,21 +87,68 @@ namespace ConsoleApp1
             string password = Console.ReadLine();
 
             LoggerService log = new LoggerService();
+            User userChangingPass = new User();
             foreach (var userce in users)
             {
                 if(userce.Username == username && userce.Password == password)
                 {
                     if(!File.Exists(directoryPath + @"\log.txt"))
                     {
-                        File.Create(directoryPath + @"\log.txt").Close();
+                        File.Create(directoryPath + @"\log.txt").Close()
+;
                         log.Log(userce.Username, userce.Password, directoryPath + @"\log.txt");
                     }
                     else
                     {
                         log.Log(userce.Username, userce.Password, directoryPath + @"\log.txt");
                     }
-                    
+
+                    int action1;
                     Console.WriteLine($"Welcome {userce.Username}!");
+                    Console.WriteLine("Choose Action:");
+                    Console.WriteLine("1. Change Password");
+                    Console.WriteLine("2. Exit");
+                    bool result1 = int.TryParse(Console.ReadLine(), out action1);
+                    if (result1)
+                    {
+                        if(action1 == 1)
+                        {
+                            userChangingPass = userce;
+                        }
+                        else if(action1 == 2)
+                        {
+                            return;
+                        }
+                    }
+                }
+            }
+            ChangePassword(userChangingPass);
+        }
+
+        public void ChangePassword(User user)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Write your new password:");
+                string input = Console.ReadLine();
+                if (user.Password == input)
+                {
+                    Console.WriteLine("The password cannot be as the old one! Press any key try again!");
+                    Console.ReadLine();
+                    continue;
+                }
+                else if (String.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("Password cannot be empty. Press any key and try again!");
+                    Console.ReadLine();
+                    continue;
+                }
+                else
+                {
+                    user.Password = input;
+                    Console.WriteLine("Password changed!");
+                    break;
                 }
             }
         }
